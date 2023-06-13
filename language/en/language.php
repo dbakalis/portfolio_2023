3bcbe47b07080adcb77e189a8b2930932a7c9f52
dbakalis <dimibaka93@gmail.com>
2023-06-13 15:32:59 +0300

    Add translations files
    PHP translations files

---
+++ language/en/language.php
@@ -0,0 +1,42 @@
+<?php
+    $lang = array(
+        'navbar' => array(
+            'home'          => 'ΗΟΜΕΠΑΓΕ',
+            'about'         => 'ABOUT ME',
+            'achievments'   => 'ACHIEVEMENTS',
+            'projects'      => 'PROJECTS',
+            'jobs'          => 'JOB HISTORY',
+            'education'     => 'EDUCATION',
+            'contact'       => 'CONTACT',
+        ),
+        'hero' => array(
+            'title'             => 'I am Bakalis Dimitrios',
+            'subtitle_typed'    => 'Web designer,IT Manager,UI/UX Designer'
+        ),
+        'about' => array(
+            'name'          => 'Fullname',
+            'age'           => 'Age',
+            'email'         => 'E-mail',
+            'phone'         => 'Phone',
+            'address'       => 'Address',
+            'about_title'   => 'About me',
+            'about_text'    => 'Text about me',
+        ),
+        'counters' => array(
+            'completed_projects'    => 'Completed projects',
+            'years_experience'      => 'Years of experience',
+            'technologiess_used'    => 'Technologies utilized',
+            'customers'             => 'Customers',
+        ),
+        'projects' => array(
+            'title' => 'Projects',
+            'visit' => 'Visit',
+        ),
+        'contact' => array(
+            'title' => 'Contact form',
+            'email' => 'E-mail',
+            'name'  => 'Fullname',
+            'text'  => 'Message',
+        )
+    );
+?>
---
+++ language/gr/language.php
@@ -0,0 +1,42 @@
+<?php
+    $lang = array(
+        'navbar' => array(
+            'home'          => 'ΑΡΧΙΚΗ',
+            'about'         => 'ΣΧΕΤΙΚΑ ΜΕ ΕΜΕΝΑ',
+            'achievments'   => 'ΚΑΤΟΡΘΩΜΑΤΑ',
+            'projects'      => 'ΕΡΓΑΣΙΕΣ',
+            'jobs'          => 'ΕΡΓΑΣΙΑ',
+            'education'     => 'ΕΚΠΑΙΔΕΥΣΗ',
+            'contact'       => 'ΕΠΙΚΟΙΝΩΝΙΑ',
+        ),
+        'hero' => array(
+            'title'             => 'Ονομάζομαι Μπακάλης Δημήτριος',
+            'subtitle_typed'    => 'Προγραμματιστής ιστοσελίδων & εφαρμογών διαδικτύου,Υπεύθυνος τμήματος IT,Σχεδιαστής UI/UX'
+        ),
+        'about' => array(
+            'name'          => 'Ονομασία',
+            'age'           => 'Ηλικία',
+            'email'         => 'E-mail',
+            'phone'         => 'Τηλέφωνο',
+            'address'       => 'Διεύθυνση',
+            'about_title'   => 'Σχετικά με εμένα',
+            'about_text'    => 'Κείμνενο σχετικά με εμένα',
+        ),
+        'counters' => array(
+            'completed_projects'    => 'Ολοκληρωμένες εργασίες',
+            'years_experience'      => 'Χρόνια εμπειρίας',
+            'technologiess_used'    => 'Πλήθος τεχνολογιών',
+            'customers'             => 'Πελάτες',
+        ),
+        'projects' => array(
+            'title' => 'Εργασίες',
+            'visit' => 'Επίσκεψη',
+        ),
+        'contact' => array(
+            'title' => 'Φόρμα επικοινωνίας',
+            'email' => 'E-mail',
+            'name'  => 'Ονοματεπώνυμο',
+            'text'  => 'Μήνυμα',
+        )
+    );
+?>
